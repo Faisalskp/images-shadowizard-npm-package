625690ce9b81753cf908e85530f839ed8bac20ee
Faisalskp <faisalsaleem000786@gmail.com>
2021-04-28 12:59:43 +0500

    shadowizard-images

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# images-shadowizard-npm-package
+# what is this?
+
+Get perfect shadows every time for all images.
+
+# Installation 
+
+`npm i shadowImages --save`
+
+Then...
+
+```
+
+## Properties
+
+shadowImages supports two(2) properties, which are optional:
+
+* *shadow_type* - _high | low_ (Defaults to low)
+* *padding*  - _boolean_ (Default to false)
+
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+function shadowImages(properties) {
+    var allImages = document.querySelectorAll('.shadowizard')
+    if (properties.shadow_type === "high") {
+        properties.shadow_type = '5px'
+    } else {
+        properties.shadow_type = '16px'
+    }
+    allImages.forEach(element => {
+        element.style.boxShadow = `11px 11px ${properties.shadow_type} 2px rgba(0,0,0,0.15)`;
+        if (properties.padding) {
+            element.style.padding = `1.1em`;
+        }
+    });
+}
+module.exports.shadowImages = shadowImages
